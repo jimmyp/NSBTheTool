a3ee09ff46a7833ca43d26c6e5d1602c289b77f0
jimmyp <mail@thejimmyp.com>
2012-09-09 21:26:51 +1000

    Print Hello World on StartUp

---
+++ CommandProcessor/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NServiceBus;
+
+namespace CommandProcessor
+{
+    public class HelloWorld : IWantToRunAtStartup
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
