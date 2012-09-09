f9679a588dab9b1c1f1b60ada946f91c9b7ef249
jimmyp <mail@thejimmyp.com>
2012-09-10 08:05:55 +1000

    Send command from web app to command processor

---
+++ CommandProcessor/CommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Messages;
+using NServiceBus;
+
+namespace CommandProcessor
+{
+    class CommandHandler : IHandleMessages<SomeCommand>
+    {
+        public void Handle(SomeCommand message)
+        {
+            Console.WriteLine("Recieved Some Command");
+        }
+    }
+}
---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Messages;
+using NServiceBus;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IBus _bus;
+
+        public HomeController(IBus bus)
+        {
+            if (bus == null) throw new ArgumentNullException("bus");
+            _bus = bus;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendCommand()
+        {
+            _bus.Send(new SomeCommand(){});
+
+            return View("Index");
+        }
+    }
+}
