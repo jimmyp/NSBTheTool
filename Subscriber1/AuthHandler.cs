b1d0e4abe562dd0e563afa8821b300dfbb5c65f4
jimmyp <mail@thejimmyp.com>
2012-09-10 21:47:41 +1000

    Add auth handler

---
+++ Subscriber1/AuthHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NServiceBus;
+
+namespace Subscriber1
+{
+    public class AuthHandler : IHandleMessages<IEvent>
+    {
+        private readonly IBus _bus;
+
+        public AuthHandler(IBus bus)
+        {
+            if (bus == null) throw new ArgumentNullException("bus");
+            _bus = bus;
+        }
+
+        public void Handle(IEvent message)
+        {
+            string username;
+            _bus.CurrentMessageContext.Headers.TryGetValue("username", out username);
+            if(username != null && username != "jim")
+                _bus.DoNotContinueDispatchingCurrentMessageToHandlers();
+        }
+    }
+}
