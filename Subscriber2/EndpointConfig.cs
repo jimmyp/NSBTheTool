35c3079be6dd3e83d1aea6eb1d75d74393a58f28
jimmyp <mail@thejimmyp.com>
2012-09-10 21:38:51 +1000

    Add second publisher using parts of subscriber1

---
+++ Subscriber2/EndpointConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NServiceBus;
+
+namespace Subscriber2
+{
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Server
+    {
+    }
+}
